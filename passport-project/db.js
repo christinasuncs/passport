ff824919b292992af1684d82d0c777049e97572a
Christina Sun <csun@bayviewglen.ca>
2024-03-06 09:19:53 -0500

    change to my db

--- passport-project/db.js
+++ passport-project/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 // Replace <password> with your MongoDB Atlas password
-const mongoURI = 'mongodb+srv://new_user:<password>@cluster0.1znnbzj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoURI = 'mongodb+srv://new_user:gdxwqcSfJ6G2m0DF@cluster0.1znnbzj.mongodb.net'
+
 
   const connectToMongoDB = async (uri) => {
     try {
